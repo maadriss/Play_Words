e64ef20e4280490411ccfdd465cc0be06645b9b6
Mahdi-PC\Mahdi <moradimahdi503@gmail.com>
2021-04-15 17:05:51 +0430

    26_1_1400
    Main, play, setting, store menu created.

---
+++ Play_Words/Assets/Scripts/Canvases_Arrangement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvases_Arrangement : MonoBehaviour
+{
+    //public Canvas help_panel_canvas;
+    //public Canvas main_menu_canvas;
+    public List<Canvas> canvases;
+    //string key = "First", value = "True";
+    
+    private void Awake()
+    {
+        // Get_Menu_Canvas();
+        Show_Menu_Canvas();
+        // Show help panel for the first time.
+        /*
+        if (PlayerPrefs.HasKey(key))
+        {
+            help_panel_canvas.enabled = false;
+            
+        }
+        else 
+        {
+            Show_Menu_Canvas();
+            help_panel_canvas.enabled = true;
+            PlayerPrefs.SetString(key, value);
+        }
+        */
+    }
+
+
+    /*
+    private void Get_Menu_Canvas()
+    {        
+        canvases.AddRange(FindObjectsOfType<Canvas>());     
+        foreach (Canvas item in canvases)
+        {
+            if (item.name == "Canvas_Main_Menu")
+            { 
+                main_menu_canvas = item;
+                canvases.Remove(item);
+                break;
+            }
+        }
+    }
+    */
+
+
+    private void Show_Menu_Canvas()
+    {
+        canvases.AddRange(FindObjectsOfType<Canvas>());
+        //main_menu_canvas.enabled = true;
+        foreach (Canvas item in canvases)
+        {
+            if (item.name == "Canvas_Main_Menu")
+            {
+                item.enabled = true;
+            }
+            else
+            {
+                item.enabled = false;
+            }
+        }
+    }
+}
---
+++ Play_Words/Assets/Tests/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Damage_Calculator
+    {
+        [Test]
+        public void Damage_Test()
+        {
+            //Act
+            Debug.Log("");
+            //Assert
+        }
+        
+    }
+}
